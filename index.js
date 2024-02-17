37b1a9e97221ec6d2f34afd8505fb8ad544abcd1
Patoll <support@mc-tetragon.de>
2024-02-17 13:21:37 +0000

    added hamburger

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 window.onload = function() {
-    const header = document.querySelector('.header');
+    const header = document.querySelector('.dowheader');
     header.classList.add('play-animation');
 }
+
+$(document).ready(function(){
+	$('.hamburger-shell').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+});
