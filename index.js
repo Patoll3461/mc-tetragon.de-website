9166da3493b11a570f3ddc2b6eb86a626aa00860
Patoll <support@mc-tetragon.de>
2024-02-17 16:24:43 +0000

    changed hamburger menu to make it look better

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+var clicked;
+clicked = false;
+
 window.onload = function() {
     const header = document.querySelector('.dowheader');
     header.classList.add('play-animation');
 }
 
-$(document).ready(function(){
-	$('.hamburger-shell').click(function(){
+function sleepm(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function loadStuff() {
+	$('.hamburger-shell').click(async function () {
+		if (!clicked){
+			$('.hamburger-shell').toggleClass('blur');
+			$('#menu').toggleClass('background');
+		}
 		$('#menu').slideToggle(300);
 		$('.top').toggleClass('rotate');
 		$('.middle').toggleClass('rotate-back');
 		$('.menu-name').toggleClass('bump');
 		$('.bg-cover').toggleClass('reveal');
+		if (clicked){
+			await sleepm(250);
+			$('#menu').toggleClass('background');
+			$('.hamburger-shell').toggleClass('blur');
+		}
+		clicked = !clicked;
 	});
-	$('.bg-cover').click(function(){
-		$('#menu').slideToggle(300);
-		$('.top').toggleClass('rotate');
-		$('.middle').toggleClass('rotate-back');
-		$('.menu-name').toggleClass('bump');
-		$('.bg-cover').toggleClass('reveal');
-	})
-});
+}
+
+$(document).ready(loadStuff);
